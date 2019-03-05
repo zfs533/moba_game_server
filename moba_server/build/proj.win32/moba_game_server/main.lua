4c0311a1ffae13f910f3640b832c798120806543
zfs533 <2415933434@qq.com>
2019-03-05 15:31:21 +0800

    add build file

---
+++ moba_server/build/proj.win32/moba_game_server/main.lua
@@ -0,0 +1,5 @@
+ print("-dsklfjsaldj------")
+ -- package.path = "H:\\Apan_WorkAbout\\duokemeng\\mytest\\server\\moba_game_server\\moba_server\\lua_script\\?.lua;" ..package.path
+ -- require "test"
+ print(package.path)
+print("hello lua1 script")
