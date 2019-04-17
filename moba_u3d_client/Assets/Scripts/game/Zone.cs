341d3acebe8d907011760358b1594667d4e086d3
zfs533 <2415933434@qq.com>
2019-04-17 17:34:53 +0800

    match and robot

---
+++ moba_u3d_client/Assets/Scripts/game/Zone.cs
@@ -0,0 +1,4 @@
+public class Zone {
+    public const int SGYD = 1;
+    public const int ASSY = 2;
+}
---
+++ moba_u3d_client/Assets/Scripts/game/home_scene/team_match.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class team_match : MonoBehaviour
+{
+    public void on_begin_match_click()
+    {
+        int zid = ugames.Instance.zid;
+        logic_service.Instance.enter_zone(zid);
+    }
+}
